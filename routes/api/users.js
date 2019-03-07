9680a34014abbbd88eebc801c311d17d56743857
choooooh <ch.oh-@hotmail.com>
2019-03-08 00:01:16 +0100

    'hej'

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,19 +4,16 @@ const User = require('../../models/users');
 
 
 router.get('/', (req, res, next) => {
-    console.log('chad');
     User.find({}).then(user => {
         res.json(user);
     }).catch(next);
 });
 
 router.get('/:id', (req, res) => {
-    const found = users.find(user => user.id === parseInt(req.params.id));
-    if (found) {
-        res.json(users.filter(user => user.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No user with the id of ${req.params.id}`});
-    }
+    User.findById({_id: req.params.id}).then(user => {
+        console.log('User found!');
+        res.json(user);
+    });
 });
 
 router.post('/', (req, res, next) => {
